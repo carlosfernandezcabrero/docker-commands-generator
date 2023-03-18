c01fd7f96d1c32c88edf2e1831e31e95b7150a3e
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-03-18 19:51:59 +0100

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css'
+	import Header from '../components/header.svelte'
 </script>
 
 <svelte:head>
 	<title>Docker Commands Generator</title>
 </svelte:head>
 
+<Header />
 <slot />
