3378e0e18eeac1cb0fd0b162c5ad5cb099babaaf
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-03-18 19:54:13 +0100

    add alias for component folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script>
-	import '../app.css'
-	import Header from '../components/header.svelte'
+  import '../app.css'
+  import Header from '$components/header.svelte'
 </script>
 
 <svelte:head>
-	<title>Docker Commands Generator</title>
+  <title>Docker Commands Generator</title>
 </svelte:head>
 
 <Header />
